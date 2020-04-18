508bf47b4be8c81df7a7f5ca064c3576b9d934cc
JMDP <juanm017@gmail.com>
2020-04-17 23:29:20 -0400

    Se agrega configuracion base para Debian 10 - Gnome

---
+++ Debian 10/Gnome/configuration.sh
@@ -0,0 +1,17 @@
+# desinstalar programas por defecto
+sudo apt purge gnome-games -y
+sudo apt purge "libreoffice*" -y
+sudo apt purge xiterm+thai -y
+sudo apt purge kasumi -y
+sudo apt purge goldendict -y
+sudo apt purge hdate-applet -y
+sudo apt purge gnome-maps -y
+sudo apt autoremove
+
+# actualizar favoritos
+gsettings set org.gnome.shell favorite-apps "['firefox-esr.desktop','org.gnome.Nautilus.desktop','org.gnome.Terminal.desktop']"
+
+# configuraciones
+gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
+gsettings set org.gnome.shell enabled-extensions "['places-menu@gnome-shell-extensions.gcampax.github.com','drive-menu@gnome-shell-extensions.gcampax.github.com','workspace-indicator@gnome-shell-extensions.gcampax.github.com']"
+gsettings set org.gnome.desktop.input-sources xkb-options "['compose:rctrl']"

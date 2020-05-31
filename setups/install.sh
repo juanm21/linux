409479a9b498ccf0fd7b38aefce527b5ec297e5f
JuanM <juanm017@gmail.com>
2020-05-31 15:43:35 -0400

    Se agrega instalador wget

--- setups/install.sh
+++ setups/install.sh
@@ -1,62 +1,64 @@
-echo '====================// Update'
+echo '==================|| Update ||==========================='
 sudo apt update
-echo '====================// Upgrade'
+echo '==================|| Upgrade ||==========================='
 sudo apt upgrade -y
 
-echo '====================// Install Timeshift'
+echo '==================|| Install Timeshift ||==========================='
 sudo apt-add-repository -y ppa:teejee2008/ppa
-sudo apt-get update
-sudo apt-get install timeshift -y
+sudo apt install timeshift -y
 
-echo '====================// Install git'
+echo '==================|| Install git ||==========================='
 sudo apt install git -y
 
-echo '====================// Install curl'
+echo '==================|| Install curl ||==========================='
 sudo apt install software-properties-common apt-transport-https curl -y
 
-echo '====================// Install XInit - requerido para instalar algun gestor de ventanas' 
+echo '==================|| Instal wget ||==========================='
+sudo apt install wget -y
+
+echo '==================|| Install XInit - requerido para instalar algun gestor de ventanas' ||=========================== 
 sudo apt install xinit -y
 
-echo '====================// Install Terminal'
+echo '==================|| Install Terminal ||==========================='
 #Terminal
 sudo apt install zsh -y 
 sudo apt install terminator -y
 
-echo '====================// Install fonts'
+echo '==================|| Install fonts ||==========================='
 #Fonts
 sudo apt install fonts-powerline -y
 sudo apt install fonts-firacode -y
 
-echo '====================// Install File Manager mc'
+echo '==================|| Install File Manager mc ||==========================='
 #File Manager (Midnight Commander Features)
 sudo apt install mc -y
 
-echo '====================// Install Process Viewer htop'
+echo '==================|| Install Process Viewer htop ||==========================='
 #Process Viewer
 sudo apt install htop -y
 
-echo '====================// Install gdebi'
+echo '==================|| Install gdebi ||==========================='
 sudo apt install gdebi -y
 
-echo '====================// Install Chromium'
+echo '==================|| Install Chromium ||==========================='
 sudo apt install chromium -y
 
-echo '===================// Install ELinks web browser - text'
+echo '==================|| Install ELinks web browser - text ||==========================='
 sudo apt install elinks -y
 
-echo '===================// Install Audio GUI'
+echo '==================|| Install Audio GUI ||==========================='
 sudo apt install alsamixergui -y
 
-echo '===================// Volume Icon (exec volumeicon)'
+echo '==================|| Volume Icon (exec volumeicon) ||==========================='
 sudo apt install volumeicon-alsa -y
 
-echo '===================// Window Transparency'
+echo '==================|| Window Transparency ||==========================='
 sudo apt install xcompmgr -y
 
-echo '===================// Autoconf (Build C)'
+echo '==================|| Autoconf (Build C) ||==========================='
 sudo apt install autoconf -y
 
-echo  '====================// Install Oh My Zsh'
+echo  '==================|| Install Oh My Zsh ||==========================='
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 
